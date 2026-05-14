2d09aebec62b5f8feeefaa392011bf0f23dfae88
LUCAS DUCATENZEILER <49980088@ort>
2026-05-14 08:19:12 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tp_02_recetify.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
